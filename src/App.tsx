721b96011fdd2fa64a4f9e61266fe7c0d51c0599
Wander Moreira <wandersantosmoreira@gmail.com>
2021-07-09 10:25:20 -0300

    fix: removed content from app

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import CoachRoutes from "./routes/coach";
 import StudentRoutes from "./routes/student";
 import DefaultRoutes from "./routes/default";
 import { GlobalStyle } from "./styles/global";
-import GreenButton from "./components/GreenButton";
-import RedButton from "./components/RedButton";
 
 const App = () => {
   let typeUser = localStorage.getItem("@typeUser") || "";
@@ -12,14 +10,8 @@ const App = () => {
     typeUser = JSON.parse(typeUser);
   }
 
-
-  const handleClick = () => {
-    console.log('oi')
-  }
-
   return (
     <>
-      <RedButton onClick={handleClick} size={'small'}>TEXTO</RedButton>
       {typeUser === "" ? (
         <DefaultRoutes />
       ) : (
