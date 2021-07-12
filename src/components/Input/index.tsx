03723fb937a2dd47a7faef0101ae07d1aef8a196
Wander Moreira <wandersantosmoreira@gmail.com>
2021-07-12 16:01:35 -0300

    fix: component refactored

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,28 +1,23 @@
 import React, { ReactNode } from 'react';
 import { OutlinedInput, FormControl, InputAdornment } from '@material-ui/core';
 import clsx from 'clsx';
-import { UseFormRegister } from 'react-hook-form';
 import { useStyles } from './styles';
 
 
 interface InputProps {
   children: ReactNode;
   label: string;
-  register: UseFormRegister<Record<string, any>>;
-  nameRegister: string;
 }
 
-function Input({children, label, register, nameRegister}: InputProps) {
+function Input({children, label, ...rest}: InputProps) {
   const classes = useStyles();
   
   return (
     <div>
      <FormControl className={clsx(classes.margin, classes.textField)} >
           <OutlinedInput
-            id={nameRegister}
-            name={nameRegister}
             placeholder={label}
-            inputRef={register}
+            {...rest}
             startAdornment={<InputAdornment position="start">{children}</InputAdornment>}
           />
       </FormControl>
