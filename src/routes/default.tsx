f50843bb2d053852785fe790c8f9a8ade8e04e7d
Wander Moreira <wandersantosmoreira@gmail.com>
2021-07-13 17:26:54 -0300

    fix: routes

--- src/routes/default.tsx
+++ src/routes/default.tsx
@@ -1,9 +1,11 @@
 import { Switch, Route } from "react-router-dom";
 import LandingPage from "../pages/LandingPage";
+import RegisterUser from "../pages/RegisterUser";
 const DefaultRoutes = () => {
   return (
     <Switch>
       <Route exact path="/" component={LandingPage} />
+      <Route path="/register" component={RegisterUser} />
     </Switch>
   );
 };
