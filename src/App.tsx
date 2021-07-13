d43f694ebf690573f8dcd4b6c8315f64b19c7aad
Matheus Paiva <matheeus_paiva@hotmail.com>
2021-07-13 09:47:41 -0300

    feat: Page workouts. style: mobile.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import CoachRoutes from "./routes/coach";
 import StudentRoutes from "./routes/student";
 import DefaultRoutes from "./routes/default";
 import { GlobalStyle } from "./styles/global";
+import Workouts from "./pages/Workouts";
 import Exercices from "./pages/Exercices";
 
 const App = () => {
