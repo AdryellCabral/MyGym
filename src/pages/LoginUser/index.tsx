eeff94919e304b4288841fa180a6bfa4282aedac
Wander Moreira <wandersantosmoreira@gmail.com>
2021-07-13 14:46:56 -0300

    fix: sizes

---
+++ src/pages/LoginUser/index.tsx
@@ -0,0 +1,29 @@
+import Login from "../../components/Login";
+import { ContainerRegister, Container } from "./styles";
+import img from "../../assets/images/undraw_personal_trainer.svg";
+// import logo from "../../assets/images/Logo_halter2-verde_sembordas.png";
+
+import { useWindowWidth } from "../../providers/WindowWidth";
+import { motion } from "framer-motion";
+
+export const LoginUser = () => {
+  const { windowWidth } = useWindowWidth();
+
+  return (
+    <Container>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.6 }}
+      >
+        <ContainerRegister>
+          {windowWidth >= 768 && <img src={img} alt="" />}
+          {console.log(windowWidth)}
+          <Login />
+        </ContainerRegister>
+      </motion.div>
+    </Container>
+  );
+};
+
+export default LoginUser;
---
+++ src/pages/LoginUser/styles.ts
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const ContainerRegister = styled.div`
+  background-color: var(--light-black);
+  height: calc(112vh - 65px);
+  display: flex;
+  align-items: center;
+
+  img {
+    width: calc(70vw - 400px);
+    max-width: 800px;
+    margin-right: 5%;
+    margin-top: 100px;
+  }
+
+  @media (min-width: 1024px) {
+    img {
+      margin-left: 10%;
+    }
+
+    padding-right: 16px;
+  }
+
+  @media (min-width: 768px) {
+    height: calc(112vh - 65px);
+  }
+
+  @media (min-width: 1366px) {
+    height: calc(112vh - 75px);
+  }
+`;
+
+export const Container = styled.div`
+  background-color: var(--light-black);
+`;
