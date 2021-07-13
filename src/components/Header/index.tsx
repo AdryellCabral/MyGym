0d99a8a334895b11a068aca89445e56ab5b1e0ce
Wander Moreira <wandersantosmoreira@gmail.com>
2021-07-13 16:29:39 -0300

    fix: resolved conflicts

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -12,7 +12,6 @@ const Header = () => {
 
   console.log(`Largura: ${windowWidth}`);
 
-<<<<<<< HEAD
   return (
     <StyledHeader windowWidth={windowWidth}>
       <Container>
@@ -26,18 +25,3 @@ const Header = () => {
 };
 
 export default Header;
-=======
-	return (
-		<StyledHeader windowWidth = { windowWidth }>
-			<Container>
-				<StyledWrapper>
-					<img src={ logo.img } alt={ logo.alt } />
-					<Navbar/>
-				</StyledWrapper>
-			</Container>
-		</StyledHeader>
-	);
-}
-
-export default Header;
->>>>>>> 5ab03d8ef9c425ce38e4baadbe1ffa49fcc66e65
