990b556bd3b8b5833829ea46a7df809f989795f5
Daniel Costa <danieldesbravador45@gmail.com>
2021-07-14 14:00:53 -0300

    feat: page students

--- src/routes/coach.tsx
+++ src/routes/coach.tsx
@@ -1,12 +1,11 @@
 import { Route, Switch } from "react-router-dom";
-import InfoStudent from "../pages/Coach/InfoStudent";
 import Students from "../pages/Coach/Students";
 
 const CoachRoutes = () => {
     return (
         <Switch>
             <Route exact path="/" component={Students} />
-            <Route path="/students/:id" component={InfoStudent} />  
+            <Route  path="/students" component={Students} />
         </Switch>
     );
 }
