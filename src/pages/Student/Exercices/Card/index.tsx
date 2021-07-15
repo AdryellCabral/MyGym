450dd0e4bcac3083ea62ce38278f05460fc0441a
Marcos Vinicius P. Vieira <mviniciuspvieira@hotmail.com>
2021-07-14 22:14:25 -0300

    fix: text leak

--- src/pages/Student/Exercices/Card/index.tsx
+++ src/pages/Student/Exercices/Card/index.tsx
@@ -1,6 +1,7 @@
 import { Container } from "./styles";
 import ModalExercice from "../ModalExercice";
 import { useState } from "react";
+import { useWindowWidth } from "../../../../providers/WindowWidth";
 
 interface CardProps {
   exercice: {
@@ -17,12 +18,26 @@ const Card = ({ exercice }: CardProps) => {
   const handleModal = (modal: boolean) => {
     setOpenModal(!modal);
   };
+  const {windowWidth} = useWindowWidth()
+
+  const setLengthStr = (str: string) => {
+    let output = "";
+    let length = windowWidth > 768 ? 190 : 140 
+    if (str.length > length) {
+      output = `${str.substring(0, length)}...`;
+    } else {
+      output = str;
+    }
+    return output;
+  };
+
+  
   return (
     <Container img={exercice.image} onClick={() => handleModal(openModal)}>
       <div className="image"></div>
       <div className="info">
         <h1>{exercice.name}</h1>
-        <p>{exercice.description}</p>
+        <p>{setLengthStr(exercice.description)}</p>
       </div>
 
       <ModalExercice
