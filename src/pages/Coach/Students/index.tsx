02e767da60f7d5fe0ad424d400470e0d0dea89f1
Wander Moreira <wandersantosmoreira@gmail.com>
2021-07-15 17:54:18 -0300

    feature: component created

--- src/pages/Coach/Students/index.tsx
+++ src/pages/Coach/Students/index.tsx
@@ -2,68 +2,93 @@ import { Container } from "./styles";
 import CardList from "./CardList";
 import Card from "./Card";
 import { useEffect, useState } from "react";
-import SearchIcon from '@material-ui/icons/Search';
+import SearchIcon from "@material-ui/icons/Search";
 import { apiMyGym } from "../../../services/api";
 import { useCoach } from "../../../providers/Coach";
+import RegisterPhisicalAssessment from "../../../components/RegisterPhisicalAssessment";
+import RegisterWorkout from "../../../components/RegisterWorkout";
+import { useUserProvider } from "../../../providers/User";
+
+interface  Aluno {
+  id: number;
+  name: string;
+  description: string;
+  img: string;
+}
+
+interface Alunos{
+  students: Aluno[];
+}
 
 const Students = () => {
+  const {
+    userProvider: { idUser, token },
+  } = useUserProvider();
+
   const [filter, setFilter] = useState("");
-  const {coachAuthInfo} = useCoach();
-  const [students, setStudents] = useState({})
+  const { coachAuthInfo } = useCoach();
+  const [students, setStudents] = useState<Alunos[]>([]);
   const [objTest] = useState([
     {
       id: 1,
       name: "João Frango",
       description: "Essa descrição é um test",
       img: "https://www.smartfit.com.br/news/wp-content/uploads/2016/06/supino-reto.jpg",
-      
     },
     {
       id: 2,
       name: "João Maromba",
       description: "Essa descrição é um test",
       img: "https://www.smartfit.com.br/news/wp-content/uploads/2016/06/supino-reto.jpg",
-      
     },
   ]);
 
   const GetStudents = () => {
     apiMyGym
-      .get(`students?coachId=${coachAuthInfo.id}`, {
+      .get(`coaches?userId=${idUser}&_embed=students`, {
         headers: {
-          Authorization: `Bearer ${coachAuthInfo.token}`,
-        }
+          Authorization: `Bearer ${token}`,
+        },
       })
-      .then((response) => setStudents(response))
-  }
+      .then((response: any) => setStudents(response.data[0].students));
+  };
 
   useEffect(() => {
     GetStudents();
-  },[])
+  }, []);
 
-  const filterStudent = () => {
-    apiMyGym
-      .get(`students?name=${filter}&coachId=${coachAuthInfo.id}`, {
-        headers: {
-          Authorization: `Bearer ${coachAuthInfo.token}`,
-        }
-      })
-      .then((response) => setStudents(response))
-  };
+  // const filterStudent = () => {
+  //   apiMyGym
+  //     .get(`students?name=${filter}&coachId=${coachAuthInfo.id}`, {
+  //       headers: {
+  //         Authorization: `Bearer ${coachAuthInfo.token}`,
+  //       },
+  //     })
+  //     .then((response) => setStudents(response));
+  // };
   return (
-    <section className="page--students" style={{ width: "100%", marginTop: "95px" }}>
+    <section
+      className="page--students"
+      style={{ width: "100%", marginTop: "95px" }}
+    >
       <Container>
         <div className="container--top">
           <h1>Alunos</h1>
           <div className="container--filter">
-            <input type="text" value={filter} onChange={e => setFilter(e.target.value)}/>
-            <button onClick={filterStudent}><SearchIcon/></button>
+            <input
+              type="text"
+              value={filter}
+              onChange={(e) => setFilter(e.target.value)}
+            />
+            <button /*onClick={filterStudent}*/>
+              <SearchIcon />
+            </button>
           </div>
         </div>
 
         <CardList>
           <>
-            {objTest.map((item) => (
+            {students.map((item: any) => (
               <Card key={item.id} student={item} />
             ))}
           </>
